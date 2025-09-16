9a0e503dd96dabcbc1f41ce5f0bcd9f482e4af63
smritig537 <smritig537@gmail.com>
2025-09-16 23:14:25 +0530

    adding changes

--- Frontend/src/Components/Body.jsx
+++ Frontend/src/Components/Body.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import { Base_Url } from '../utils/Constants'
+import { useDispatch } from 'react-redux'
+import { addUser } from '../utils/userSlice';
+import axios from 'axios'
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const fetchUser = async () => {
+    try{
+       const res = await axios.get(Base_Url+'/profile', {
+      withCredentials: true
+    });
+    dispatch(addUser(res.data));
+    }catch(error){
+      Navigate('/login');
+      console.log(error);
+    }  
+  }
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
   return (
     <>
     <Navbar/>
